72fffe601f307344ff316df13f87e74495293e99
ignat <ignatlebedef@gmail.com>
2024-09-26 13:33:48 +0300

    fixed length added

--- Generator.cpp
+++ Generator.cpp
@@ -4,7 +4,7 @@
 
 void generator(char a[], int &len) {
     std::mt19937 rnd(std::random_device{}());
-    len = std::uniform_int_distribution<int>(1, 10)(rnd);
+    len = std::uniform_int_distribution(1, 10)(rnd);
 
     char digits[10];
     for (char i = '0'; i <= '9'; ++i) {
@@ -18,14 +18,32 @@ void generator(char a[], int &len) {
             a[i] = '\0';
         }
     }
+
     std::shuffle(a, a + len, rnd);
 }
 
+void generatorWithFixedLen(char a[], int len) {
+    std::mt19937 rnd(std::random_device{}());
+
+    char digits[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    for (int i = 9; i > 0; --i) {
+        int j = std::uniform_int_distribution<int>(0, i)(rnd);
+        std::swap(digits[i], digits[j]);
+    }
+
+    for (int i = 0; i < len; ++i) {
+        a[i] = digits[i];
+    }
+}
+
 int main() {
+    int len = 0;
+    std::cout << "Enter length of generated sets (from 0 to 10):\n";
+    std::cin >> len;
     for (int i = 0; i < 4; ++i) {
         char a[10];
-        int len = 0;
-        generator(a, len);
+        generatorWithFixedLen(a, len);
 
         for (int j = 0; j < len; ++j) {
             if (a[j] != '\0') {
