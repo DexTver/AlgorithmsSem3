de94bd58e668d8c9b53a90162ce2f87327c29b37
User. <ivan.sharapov.05@yandex.ru>
2024-09-27 14:56:13 +0300

    OH WOW

--- AutoTest.cpp
+++ AutoTest.cpp
@@ -1,7 +1,8 @@
-#include <cstdlib>
-#include <chrono>
-#include <iostream>
-#include <fstream>
+//#include <cstdlib>
+//#include <chrono>
+//#include <iostream>
+//#include <fstream>
+#include <bits/stdc++.h>
 
 void out_print(const char name[]) {
     std::string line;
@@ -27,36 +28,39 @@ int main() {
     system("g++ Bits.cpp -o Bits");
     system("g++ MWord.cpp -o MWord");
 #endif
-
+    freopen("logs.txt", "w", stdout);
     // running
-    for (int i = 1; i <= 11; ++i) {
-        if (i < 11) system(std::format("Generator {} > input.txt", i).c_str());
-        else system("Generator > input.txt");
+    for (int tt = 0; tt < 100; ++tt) {
+        for (int i = 1; i <= 10; ++i) {
+            if (i < 11) system(std::format("Generator {} > input.txt", i).c_str());
+            else system("Generator > input.txt");
 
-        std::cout << "\n\nTest " << i << ":\n";
-        std::ifstream file("input.txt");
-        std::string line;
-        while (std::getline(file, line)) {
-            std::cout << line << "\n";
+//            std::cout << "\n\nTest " << i << ":\n";
+//            std::ifstream file("input.txt");
+//            std::string line;
+//            while (std::getline(file, line)) {
+//                std::cout << line << "\n";
+//            }
+//            file.close();
+
+            system("Array < input.txt > out_array.txt");
+            std::cout << i << "\n";
+            std::cout << "Array result: ";
+            out_print("out_array.txt");
+
+            system("List < input.txt > out_list.txt");
+            std::cout << "\nList result:  ";
+            out_print("out_list.txt");
+
+            system("Bits < input.txt > out_bits.txt");
+            std::cout << "\nBits result:  ";
+            out_print("out_bits.txt");
+
+            system("MWord < input.txt > out_mword.txt");
+            std::cout << "\nMWord result: ";
+            out_print("out_mword.txt");
+            std::cout << "\n";
         }
-        file.close();
-
-        system("Array < input.txt > out_array.txt");
-        std::cout << "Array result: ";
-        out_print("out_array.txt");
-
-        system("List < input.txt > out_list.txt");
-        std::cout << "\nList result:  ";
-        out_print("out_list.txt");
-
-        system("Bits < input.txt > out_bits.txt");
-        std::cout << "\nBits result:  ";
-        out_print("out_bits.txt");
-
-        system("MWord < input.txt > out_mword.txt");
-        std::cout << "\nMWord result: ";
-        out_print("out_mword.txt");
     }
-
     return 0;
 }
