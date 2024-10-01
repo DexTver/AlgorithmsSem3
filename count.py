955d206d4b1de42a00dfb0029c25240ac7478336
ignat <ignatlebedef@gmail.com>
2024-10-01 14:36:38 +0300

    incredible logs processing

--- count.py
+++ count.py
@@ -1,50 +1,10 @@
-array_time = [0 for i in range(10)]
-list_time = [0 for i in range(10)]
-bits_time = [0 for i in range(10)]
-mword_time = [0 for i in range(10)]
-
-mode = 1
-
-with open("cmake-build-debug/logs.txt", mode="r", encoding="utf-8") as f:
-    a = list(f.readlines())
-    for s in a:
-        x = s.split()
-        if len(x) == 1:
-            mode = int(x[0])
-        else:
-            if x[0] == "Array":
-                if array_time[mode - 1] == 0:
-                    array_time[mode - 1] = int(x[-2])
-                else:
-                    array_time[mode - 1] = (int(x[-2]) + array_time[mode - 1]) // 2
-            if x[0] == "List":
-                if list_time[mode - 1] == 0:
-                    list_time[mode - 1] = int(x[-2])
-                else:
-                    list_time[mode - 1] = (int(x[-2]) + list_time[mode - 1]) // 2
-            if x[0] == "Bits":
-                if bits_time[mode - 1] == 0:
-                    bits_time[mode - 1] = int(x[-2])
-                else:
-                    bits_time[mode - 1] = (int(x[-2]) + bits_time[mode - 1]) // 2
-            if x[0] == "MWord":
-                if mword_time[mode - 1] == 0:
-                    mword_time[mode - 1] = int(x[-2])
-                else:
-                    mword_time[mode - 1] = (int(x[-2]) + mword_time[mode - 1]) // 2
-
-for x in array_time:
-    print(x, end='\t')
-print()
-
-for x in list_time:
-    print(x, end='\t')
-print()
-
-for x in bits_time:
-    print(x, end='\t')
-print()
-
-for x in mword_time:
-    print(x, end='\t')
-print()
+with open("logs.txt", "r") as f:
+    a = [line for line in f]
+    s = 0
+    for k in range(10):
+        for i in range(1 + 5 * k, 5 + 5 * k):
+            for j in range(i, len(a), 5 * 10):
+                s += int(a[j].split()[-2])
+            print(s // (len(a) // 50), end=" ")
+            s = 0
+        print()
